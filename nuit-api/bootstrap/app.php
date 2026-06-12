0e16faf98f3054484ec012bf2a8f010e825e72cc
Ahmed Ragab <ahmed.ragab2913@gmail.com>
2026-06-13 02:14:57 +0300

    Fix Sentry integration import error

--- nuit-api/bootstrap/app.php
+++ nuit-api/bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
-use Sentry\Laravel\Integration;
+
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -26,5 +26,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('sanctum:prune-expired --hours=8')->daily();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        Integration::handles($exceptions);
+        
     })->create();
